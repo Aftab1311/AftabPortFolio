4f8a6dd08dcb64214c716ff1536710f51c0b4964
abc <abc@example.com>
2023-12-20 23:04:49 +0530

    Hero section Completed with navbar made responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import "./App.css";
+import hand from "./wave.png";
+import { useState } from "react";
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <div className="App">
       <nav className="navbar">
-        <h3 className="logo">Aftab.Dev</h3>
-        <ul>
+        <h3 className="logo">AFTAB.DEV</h3>
+        <span className="menu-button" onClick={toggleMenu}>
+          {!isMenuOpen ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M4 6l16 0"></path>
+              <path d="M4 12l16 0"></path>
+              <path d="M4 18l16 0"></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M18 6l-12 12"></path>
+              <path d="M6 6l12 12"></path>
+            </svg>
+          )}
+        </span>
+        <ul className="desktop-menu">
           <li>
             <a href="#home">Home</a>
           </li>
@@ -18,28 +60,126 @@ function App() {
           <li>
             <a href="#contact">Contact</a>
           </li>
-          <li>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="30"
-              height="30"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="mobile-menu"
-            >
-              <path d="M4 6l16 0"></path>
-              <path d="M4 12l16 0"></path>
-              <path d="M4 18l16 0"></path>
-            </svg>
-          </li>
         </ul>
       </nav>
-      {/* <div class="mobile-nav closed-menu"><span><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="tabler-icon tabler-icon-x"><path d="M18 6l-12 12"></path><path d="M6 6l12 12"></path></svg></span><ul><li><a href="#home">Home</a></li><li><a href="#about">About</a></li><li><a href="#projects">Projects</a></li><li><a href="#contact">Contact</a></li></ul></div> */}
-     
+      {isMenuOpen && (
+        <div className="mobile-nav">
+          <ul>
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      )}
+
+      <section id="home" className="hero">
+        <div className="container">
+          <div className="content">
+            <div className="hero-main">
+              <div className="hero-text">
+                <h1>Front-End React Developer </h1>
+                <img
+                  src={hand}
+                  alt="waving_hand"
+                />
+                <p>
+                  Hi, I'm Aftab Ahmed. A passionate Front-end React Developer
+                  based in Ghaziabad, India. 📍
+                </p>
+                <span>
+                  <a
+                    aria-label="linkedin"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://www.linkedin.com/in/aftabahmed1311/"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="tabler-icon tabler-icon-brand-linkedin"
+                    >
+                      <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"></path>
+                      <path d="M8 11l0 5"></path>
+                      <path d="M8 8l0 .01"></path>
+                      <path d="M12 16l0 -5"></path>
+                      <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
+                    </svg>
+                  </a>
+                  <a
+                    aria-label="github"
+                    rel="noreferrer"
+                    target="_blank"
+                    href="https://github.com/Aftab1311/"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="tabler-icon tabler-icon-brand-github"
+                    >
+                      <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+                    </svg>
+                  </a>
+                </span>
+              </div>
+              <div className="hero-img"></div>
+            </div>
+            <div className="skills">
+              <p>Tech Stack</p>
+              <div className="logos">
+                <ul>
+                  <li>
+                    <img
+                      src="https://skillicons.dev/icons?i=html,css"
+                      alt="skill-icon"
+                    />
+                  </li>
+                  <li>
+                    <img
+                      src="https://skillicons.dev/icons?i=js,ts"
+                      alt="skill-icon"
+                    />
+                  </li>
+                  <li>
+                    <img
+                      src="https://skillicons.dev/icons?i=react,next"
+                      alt="skill-icon"
+                    />
+                  </li>
+                  <li>
+                    <img
+                      src="https://skillicons.dev/icons?i=tailwind,scss"
+                      alt="skill-icon"
+                    />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
